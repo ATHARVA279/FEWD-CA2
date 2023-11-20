d64a79c539a1121fcc3c0b72a41659470ebec5a1
ATHARVA279 <atharvak6363@gmail.com>
2023-11-20 23:57:40 +0530

    added

---
+++ index.js
@@ -0,0 +1,15 @@
+const InstBtn = document.getElementById("InstBtn")
+const PlayBtn = document.getElementById("PlayBtn")
+const GitBtn = document.getElementById("GitBtn")
+
+InstBtn.onclick=()=>{
+    window.location.href = "./Instruction.html"
+}
+
+PlayBtn.onclick=()=>{
+    window.location.href = "./Story.html"
+}
+
+GitBtn.onclick=()=>{
+    window.location.href = "https://www.linkedin.com/in/atharva-kharade-604944287/"
+}
